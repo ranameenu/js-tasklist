d8550f2988033d1c22c615ac43f5ab0390f7f2b0
meenu rana <meenurana32@gmail.com>
2019-09-05 22:30:13 +0530

    1.0

---
+++ index.js
@@ -0,0 +1,58 @@
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+//load all event listeners
+loadEventListeners();
+
+//load event listeners
+
+function loadEventListeners()
+{
+//add task form
+form.addEventListener('submit', addTask);
+}
+//addTask function
+function addTask(e)
+{
+  if(taskInput.value === '')
+  {
+     alert('Add a Task');
+  } 
+   //create li element
+  const li=document.createElement('li');
+  //add class
+  li.className='collection-item';
+
+  //create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+
+  //create new link element
+  const link= document.createElement('a');  
+
+  //Add class
+   link.className='delete-item secondary-content';
+
+   //Add icon Html
+   link.innerHTML = '<i class="fa fa-remove"></i>';
+
+   //Append the link to li
+   li.appendChild(link);
+
+   //Append li to ul
+   taskInput.value = '';
+   console.log(li);
+
+
+
+
+
+
+
+
+
+  e.preventDefault();
+
+}
