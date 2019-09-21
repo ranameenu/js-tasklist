238b3abf8d3dfff2e5f65e643fdd019f8703b51b
meenu rana <meenurana32@gmail.com>
2019-09-21 21:25:07 +0530

    1.2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
+const clearsBtn = document.querySelector('.clear-task');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -9,50 +9,39 @@ loadEventListeners();
 
 //load event listeners
 
-function loadEventListeners()
-{
-//add task form
-form.addEventListener('submit', addTask);
+function loadEventListeners() {
+  //add task form
+  form.addEventListener('submit', addTask);
 }
 //addTask function
-function addTask(e)
-{
-  if(taskInput.value === '')
-  {
-     alert('Add a Task');
-  } 
-   //create li element
-  const li=document.createElement('li');
+function addTask(e) {
+  if (taskInput.value === '') {
+    alert('Add a Task');
+  }
+  //create li element
+  const li = document.createElement('li');
   //add class
-  li.className='collection-item';
+  li.className = 'collection-item';
 
   //create text node and append to li
   li.appendChild(document.createTextNode(taskInput.value));
 
   //create new link element
-  const link= document.createElement('a');  
+  const link = document.createElement('a');
 
   //Add class
-   link.className='delete-item secondary-content';
+  link.className = 'delete-item secondary-content';
 
-   //Add icon Html
-   link.innerHTML = '<i class="fa fa-remove"></i>';
+  //Add icon Html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
 
-   //Append the link to li
-   li.appendChild(link);
+  //Append the link to li
+  li.appendChild(link);
 
-   //Append li to ul
-   taskInput.value = '';
-   console.log(li);
+  //Append li to ul
+  taskList.appendChild(li);
 
-
-
-
-
-
-
-
-
+  taskInput.value = '';
+  console.log(li);
   e.preventDefault();
-
 }
